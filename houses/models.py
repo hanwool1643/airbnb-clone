ee0aabe226647f9132cdd14470867ac75006a60a
han <hanwe92@gmail.com>
2022-11-08 20:09:29 +0900

    #4 DJANGO APPS

---
+++ houses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    """Model Definition for Houses"""
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True, help_text="Does this gouse allow pets?")
+
+    def __str__(self):
+        return self.name
