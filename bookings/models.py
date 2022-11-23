8765ab2368cfaad900a74405ccf600e576931f81
han <hanwe92@gmail.com>
2022-11-23 20:42:05 +0900

    #6.7~6.9

---
+++ bookings/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Booking(CommonModel):
+
+    """Booking Model Definition"""
+
+    class BookingKindChoices(models.TextChoices):
+        ROOM = "room", "Room"
+        EXPERIENCE = "experience", "Experience"
+
+    kind = models.CharField(
+        max_length=15,
+        choices=BookingKindChoices.choices,
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+
