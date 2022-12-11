4b7302a129cde986b2694a2d05ac1181bccdc3bf
han <hanwe92@gmail.com>
2022-12-12 07:45:52 +0900

    #8.3(2) StarFilter

--- reviews/admin.py
+++ reviews/admin.py
@@ -6,11 +6,11 @@ class WordFilter(admin.SimpleListFilter):
 
     title = "Filter by words"
 
-    parameter_name = "word"
+    parameter_name = "word" #url
 
     def lookups(self, request, model_admin):
         return [
-            ("good", "Good"),
+            ("good", "Good"), #첫번째는 url, 두번째는 어드민패널에 표시
             ("awesome", "Awesome"),
             ("great", "Great"),
         ]
@@ -22,6 +22,23 @@ class WordFilter(admin.SimpleListFilter):
         else:
             return reviews
 
+class StarFilter(admin.SimpleListFilter):
+    title = "Filter by stars"
+    parameter_name = "start"
+
+    def lookups(self, requset, model_admin):
+        return [
+            ("bad", "Bad"),
+            ("good", "Good"),
+        ]
+    def queryset(self, request, queryset): #self는 class 자체, queryset은 reviews 모두
+        star = self.value() # lookups에서 선택한 필터종류
+        if star == "bad":
+            return queryset.filter(rating__lt=3)
+        elif star == "good":
+            return queryset.filter(rating__gte=3)
+        else:
+            return queryset
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
@@ -31,6 +48,7 @@ class ReviewAdmin(admin.ModelAdmin):
     )
     list_filter = (
         WordFilter,
+        StarFilter,
         "rating",
         "user__is_host",
         "room__categories",
