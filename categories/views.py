c298f3b07c42e9a8109c6bf057ec69531666e820
han <hanwe92@gmail.com>
2022-12-19 20:34:52 +0900

    #10.2~10.4

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,20 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
+@api_view(["GET","POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
-        {
-            "ok": True,
-            "categories": all_categories,
-        }
-    )
-# Create your views here.
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
