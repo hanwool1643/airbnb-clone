0dd423d21378a0b15ab544a650a9a69f7d72662e
han <hanwe92@gmail.com>
2022-12-25 11:15:37 +0900

    #10.12 ModelViewSet

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,4 +5,4 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = "__all__" # 다른 옵션 exclude = ("name", "kind") or fields = ("name", "kind") 
